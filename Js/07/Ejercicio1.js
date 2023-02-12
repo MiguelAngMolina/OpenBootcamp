fbf63797b71868c8297f360eaf8a714644ea74ce
miguelAngel <miguel11881@gmail.com>
2023-02-12 16:25:09 -0500

    Ejercicio1- Set

---
+++ Js/07/Ejercicio1.js
@@ -0,0 +1,10 @@
+const Fam = ["Miguel Angel", "Juan David", "Mafe", "Diana", "Rey", "Paca"]
+
+const NewSet = new Set(Fam)
+console.log(NewSet)
+
+NewSet.add("Miguel Angel")
+console.log(NewSet)
+
+NewSet.add("JavaScript")
+console.log(NewSet)
